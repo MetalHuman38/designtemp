bc0a50290a7fe389a34318a9ace312978c10ba67
Babatunde Kalejaiye <kalejaib@roehampton.ac.uk>
2024-11-09 18:32:36 +0000

    postgres-test

--- app/uidir/views.py
+++ app/uidir/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     """
@@ -7,3 +8,4 @@ def home(request):
     """
 
     return render(request, 'uidir/home.html')
+
